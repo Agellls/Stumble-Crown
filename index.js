d2e5737c331ec23ee2d15d33a7fae53ae5b3e8ee
NexMou <69827604+NexMou@users.noreply.github.com>
2022-06-28 00:12:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,69 +1,223 @@
+
+
 const fetch = require('node-fetch');
 const moment = require('moment');
 const chalk = require('chalk');
 const rs = require('readline-sync');
+const delay = require('delay');
 
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
+const GoStumble = (code, auth) => new Promise((resolve, reject) => {
 
-const GoStumble = (auth) => new Promise((resolve, reject) => {
-
-  fetch('http://kitkabackend.eastus.cloudapp.azure.com:5010/round/finishv2/3', {
-    method: 'GET',
-    headers: {
-      'authorization': auth
-    }
-  })
+    fetch(`http://kitkabackend.eastus.cloudapp.azure.com:5010/round/finishv2/${code}`, {
+        method: 'GET',
+        headers: {
+            'authorization': auth
+        }
+    })
     .then(res => res.text())
-    .then(data => {
-      resolve(data);
+    .then(data=> {
+        resolve(data);
     })
     .catch(err => {
-      reject(err);
+        reject(err);
     });
 
 });
 
 (async () => {
 
-  console.log(`
-███╗   ██╗ ███████╗ ██╗  ██╗ ███╗    ╔███  ██████╗  ██║   ██╗
-████╗  ██║ ██╔════╝ ╚██╗██╔╝ ████╗  ╔████ ██╔═══██╗ ██║   ██║
-██╔██╗ ██║ █████╗    ╚███╔╝  ██╔██╗╔██╗██ ██║   ██║ ██║   ██║
-██║╚██╗██║ ██╔══╝    ██╔██╗  ██║╚████╝║██ ██║   ██║ ██║   ██║
-██║ ╚████║ ███████╗ ██╔╝ ██╗ ██║ ║██║ ║██ ╚██████╔╝ ╚██████╔╝
-╚═╝  ╚═══╝ ╚══════╝ ╚═╝  ╚═╝ ╚═╝ ╚══╝ ╚═╝  ╚═════╝   ╚═════╝
+    console.log(`███████╗████████╗██╗   ██╗███╗   ███╗██████╗ ██╗     ███████╗
+██╔════╝╚══██╔══╝██║   ██║████╗ ████║██╔══██╗██║     ██╔════╝
+███████╗   ██║   ██║   ██║██╔████╔██║██████╔╝██║     █████╗  
+╚════██║   ██║   ██║   ██║██║╚██╔╝██║██╔══██╗██║     ██╔══╝  
+███████║   ██║   ╚██████╔╝██║ ╚═╝ ██║██████╔╝███████╗███████╗
+╚══════╝   ╚═╝    ╚═════╝ ╚═╝     ╚═╝╚═════╝ ╚══════╝╚══════╝
+                                                             
+    ██████╗ ██╗   ██╗███████╗ █████╗ ██╗  ██╗                
+    ██╔══██╗██║   ██║██╔════╝██╔══██╗██║ ██╔╝                
+    ██████╔╝██║   ██║███████╗███████║█████╔╝                 
+    ██╔══██╗██║   ██║╚════██║██╔══██║██╔═██╗                 
+    ██║  ██║╚██████╔╝███████║██║  ██║██║  ██╗                
+    ╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝                               
+By : ${chalk.red('@astargans')} - ${chalk.blue('galaucrew.my.id')} - ${chalk.yellow('@Adi_Nugroho')}
+Features :
+1. ${chalk.magenta('Reward : 1 Crown + 30 Trophy + 35 Star ( Stumble Pass ) + 3000 EXP')}
+2. ${chalk.magenta('Reward : 20 Trophy + 30 Star ( Stumble Pass ) + 1500 EXP')}
+3. ${chalk.magenta('Reward : 10 Trophy + 25 Star ( Stumble Pass ) + 500 EXP')}
+4. ${chalk.magenta('Reward : 20 Star ( Stumble Pass ) + 100 EXP')}
 
-${chalk.green('Type : Recode By NexMou')}
-By : ${chalk.red('CopyRight by @Only_NoRealss')}
+pilihlah hadiah sesuai dengan apa yang kamu dapatkan pada game jika sembarangan akun kamu akan terkena autoban.
+
+${chalk.red('Warning!')}
+${chalk.green('ID: Jangan Gunakan Delay 1000-4000 Karna Akun Kalian Akan Terkena Ban')}
+
+
 `);
 
-  const auth = rs.question('[+] Enter Authentication Code! : ');
-  console.log('');
+    
+    const feature = rs.question('[+] Enter feature needed : ');
 
-  while (true) {
+    if (feature == '1') {
 
+        const auth = rs.question('[+] Enter your auth token : ');
+        const time = rs.question('[+] Enter Delay in milisecond ( Ex : 6000 = 6 sec ) : ');
+        console.log('');
 
-    const result = await GoStumble(auth);
-    if (!result) {
+        while (true) {
 
-    } else if (result.includes('User')) {
+            var code = '3';
+            const result = await GoStumble(code, auth);
+            if (!result) {
 
-      const data = JSON.parse(result);
-      const username = data.User.Username;
-      const country = data.User.Country;
-      const trophy = data.User.SkillRating;
-      const crown = data.User.Crowns;
+                console.log(chalk.red(`[ ${moment().format('HH:mm:ss')} ] Wrong cookie or Expired Cookie Try Again!`));
+                break;
 
-console.log(chalk.blue(`\r[ ${moment().format('HH:mm:ss')} ] | ${chalk.green(`User : ${username}`)} | ${chalk.yellow(`Trophy : ${trophy}`)} | ${chalk.red(`Crown : ${crown}`)} |`));
-      await sleep(6000);
+            } else if (result.includes('User')) {
 
-    } else if (result == 'BANNED') {
-      console.log(chalk.bgRed(`Your Account has been Banned`));
-     break;
+                const data = JSON.parse(result);
+                const username = data.User.Username;
+                const country = data.User.Country;
+                const exp = data.User.Experience;
+                const tokenPass = data.User.BattlePass.PassTokens;
+                const trophy = data.User.SkillRating;
+                const crown = data.User.Crowns;
+                
+                console.log(chalk.green(`[ ${moment().format('HH:mm:ss')} ] Nickname : ${username} | Country : ${country} | ${chalk.blue(`EXP : ${exp}`)} | ${chalk.blue(`Pass Star : ${tokenPass}`)} | ${chalk.blue(`Trophy : ${trophy}`)} | ${chalk.blue(`Crown : ${crown}`)}`));
+                await delay(time)
+
+            } else if (result == 'BANNED') {
+
+                console.log(chalk.red(`[ ${moment().format('HH:mm:ss')} ] Banned By KITKA !`));
+                break;
+                
+            } else if (result == 'SERVER_ERROR') {
+
+                continue;
+                
+            }
+        }
+        
+    } else if (feature == '2') {
+
+        const auth = rs.question('[+] Enter your auth token : ');
+        const time = rs.question('[+] Enter Delay in milisecond  ( Ex : 6000 = 6 second ) : ');
+        console.log('');
+
+        while (true) {
+
+            var code = '2';
+            const result = await GoStumble(code, auth);
+            if (!result) {
+
+                console.log(chalk.red(`[ ${moment().format('HH:mm:ss')} ] Wrong cookie or Expired cookie !`));
+                break;
+
+            } else if (result.includes('User')) {
+
+                const data = JSON.parse(result);
+                const username = data.User.Username;
+                const country = data.User.Country;
+                const exp = data.User.Experience;
+                const tokenPass = data.User.BattlePass.PassTokens;
+                const trophy = data.User.SkillRating;
+                
+                console.log(chalk.green(`[ ${moment().format('HH:mm:ss')} ] Nickname : ${username} | Country : ${country} | ${chalk.blue(`EXP : ${exp}`)} |  ${chalk.blue(`Pass Star : ${tokenPass}`)}  | ${chalk.blue(`Trophy : ${trophy}`)}`));
+                await delay(time)
+                
+            } else if (result == 'BANNED') {
+
+                console.log(chalk.red(`[ ${moment().format('HH:mm:ss')} ] Banned By KITKA!!!`));
+                break;
+                
+            } else if (result == 'SERVER_ERROR') {
+
+                continue;
+                
+            }
+        }
+
+    } else if (feature == '3') {
+
+        const auth = rs.question('[+] Enter your auth token : ');
+        const time = rs.question('[+] Enter Delay in milisecond ( Ex : 6000 = 6 sec ) : ');
+        console.log('');
+
+        while (true) {
+
+            var code = '1';
+            const result = await GoStumble(code, auth);
+            if (!result) {
+
+                console.log(chalk.red(`[ ${moment().format('HH:mm:ss')} ] Wrong cookie or Expired cookie !`));
+                break;
+
+            } else if (result.includes('User')) {
+
+                const data = JSON.parse(result);
+                const username = data.User.Username;
+                const country = data.User.Country;
+                const exp = data.User.Experience;
+                const tokenPass = data.User.BattlePass.PassTokens;
+                const trophy = data.User.SkillRating;
+                
+                console.log(chalk.green(`[ ${moment().format('HH:mm:ss')} ] Nickname : ${username} | Country : ${country} | ${chalk.blue(`EXP : ${exp}`)} |  ${chalk.blue(`Pass Star : ${tokenPass}`)}  | ${chalk.blue(`Trophy : ${trophy}`)}`));
+                await delay(time)
+                
+            } else if (result == 'BANNED') {
+
+                console.log(chalk.red(`[ ${moment().format('HH:mm:ss')} ] Banned !`));
+                break;
+                
+            } else if (result == 'SERVER_ERROR') {
+
+                continue;
+                
+            }
+        }
+
+    } else if (feature == '4') {
+
+        const auth = rs.question('[+] Enter your auth token : ');
+        const time = rs.question('[+] Enter Delay in milisecond ( Ex : 6000 = 6 sec ) : ');
+        console.log('');
+        
+        while (true) {
+
+            var code = '0';
+            const result = await GoStumble(code, auth);
+            if (!result) {
+
+                console.log(chalk.red(`[ ${moment().format('HH:mm:ss')} ] Wrong cookie or Expired cookie !`));
+                break;
+
+            } else if (result.includes('User')) {
+
+                const data = JSON.parse(result);
+                const username = data.User.Username;
+                const country = data.User.Country;
+                const exp = data.User.Experience;
+                const tokenPass = data.User.BattlePass.PassTokens;
+                
+                console.log(chalk.green(`[ ${moment().format('HH:mm:ss')} ] Nickname : ${username} | Country : ${country} | ${chalk.blue(`EXP : ${exp}`)} | ${chalk.blue(`Pass Star : ${tokenPass}`)}`));
+                await delay(time)
+                
+            } else if (result == 'BANNED') {
+
+                console.log(chalk.red(`[ ${moment().format('HH:mm:ss')} ] Banned By KITKA !`));
+                break;
+                
+            } else if (result == 'SERVER_ERROR') {
+
+                continue;
+                
+            }
+        }
+
+    } else {
+
+        console.log(chalk.red(`\n[ ${moment().format('HH:mm:ss')} ] Wrong feature !`));
+
     }
-  }
-
+    
 
 })();
